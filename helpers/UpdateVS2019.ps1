ab43d97fa94e4efd09497625defc4f4f156e07e4
Leonardo Tatsch Correa TIMESTAMP <LeonardoTatschCorrea.TIMESTAMP@edp.com>
2021-08-26 17:06:41 +0100

    Changed ps1 script to update comm VS2019

--- helpers/UpdateVS2019.ps1
+++ helpers/UpdateVS2019.ps1
@@ -98,4 +98,7 @@ $vsOptions = @(
 
 Start-Process -FilePath $filePath update -ArgumentList $vsOptions -Wait
 Start-Sleep -s 5
+Invoke-WebRequest https://download.visualstudio.microsoft.com/download/pr/014120d7-d689-4305-befd-3cb711108212/0307177e14752e359fde5423ab583e43/ndp48-devpack-enu.exe -OutFile C:\ndp48-devpack-enu.exe
+Start-Sleep -s 5
+Start-Process C:\ndp48-devpack-enu.exe -ArgumentList "/q /norestart" -Wait
 Restart-Computer
